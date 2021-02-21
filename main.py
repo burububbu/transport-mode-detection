d07c10a80b61db9cab5ef799c7e7bba2379e0d45
burububbu <aless97b@gmail.com>
2021-02-21 12:37:51 +0100

    modified class, to fix the target question

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 from tddataset import TDDataset
-import pandas as pd
 
-d = None
+df = None
 dt = None
 path = '../dataset/dataset_5secondWindow.csv'
 excluded_sensor = ['light', 'pressure', 'magnetic_field','gravity','proximity']
 # and magnetic_field_uncalibrated
+
+#features
+# 	accelerometer,
+# 	game_rotation_vector,
+# 	gravity,
+# 	gyroscope,
+# 	gyroscope_uncalibrated,
+# 	linear_acceleration,
+# 	orientation,
+# 	rotation_vector,
+# 	step_counter,
+# 	sound,
+# 	speed,
+
     
     # every sensor generates 4 features: mean, min, max, std
     # 64/4 = 16 sensors
@@ -21,16 +34,26 @@ excluded_sensor = ['light', 'pressure', 'magnetic_field','gravity','proximity']
 
 
 def main():
-    global d
+    global df
     global dt
+    # dataset instance
+    dt = TDDataset(path, excluded_sensor)
+    random_forest()
     
-    dtt = TDDataset()
     
-    dtt.load_dataset_from_csv(path, excluded_sensor)
-    
-    d = dtt.get_data()
-    print(d.columns.values)
+def random_forest():
+    global df
+    global dt
+    df = dt.get_data_feat(['accelerometer', 'gyroscope'])
+    RF_preprocessing()
 
+
+# serie of functions to handle different types of prprocessing based on the type of model
+def RF_preprocessing():
+    global df
+    df.dropna(inplace=True)
+    print(df.head().describe())
+    
 if __name__ == '__main__':
     main()
     
--- tddataset.py
+++ tddataset.py
@@ -1,38 +1,53 @@
 import pandas as pd
+import re
 # TODO
 class TDDataset:
-    """Transport detection dataset with sensor data"""
-    # variables 
-    def __init__(self):
-        # without target
-        self.data = None
-        self.target = None
-    
-    def load_dataset_from_csv(self, csv_path, excluded_sensors=[]):
+    """
+    Transport detection dataset with sensor data.
+    Fields:
+        - feat
+        - data
+        - target
+        -                
+    """
+    def __init__(self, csv_path, excluded_sensors=[]):
+        feat_list = self._analyze_feat(csv_path, excluded_sensors)
+        self.data, self.target = self._load_dataset_from_csv(csv_path, feat_list)
+        
+    def _load_dataset_from_csv(self, csv_path, feat_list):
         """Load dataset from cvs file"""
-        if excluded_sensors:
-            to_exclude = self._analyze_feat(csv_path, excluded_sensors)
-            self.data = pd.read_csv(csv_path, usecols = lambda n: n not in to_exclude)
-        else:
-            self.data = pd.read_csv(csv_path)
-
-        self.data = self.data.iloc[:, 5:-2]
+        data = pd.read_csv(csv_path, usecols = feat_list)
+        target = pd.read_csv(csv_path, sep=',', usecols = ['target'])
         
-        self.target = pd.read_csv(csv_path, sep=',', usecols = ['target'])
-        
-    def get_data(self, features=[]):
+        return data, target
+    
+    def get_data_feat(self, sensors = []):
         """Get entire dataset or, if specified, return specific columns"""
-        # TODO
-        return self.data
+        indexes = []
+        
+        for s in sensors:
+            ind = self._feat.index(s) * 4
+            indexes.extend(list(range(ind, ind + 4)))
+        
+        return self.data.iloc[:, indexes]
     
     def _analyze_feat(self, csv_path, excluded_sensors):
-        """ create list of names to exclude"""
-        temp = pd.read_csv(csv_path, header=None, nrows=1).iloc[:, 5:-2]
+        """create list of names to exclude"""
+        feat_names = pd.read_csv(csv_path, header=None, nrows=1).iloc[:, 5:-2]
         # for t in temp.values[0]:
         #     for s in excluded_sensors:
         #         if s in t:
         #             to_exclude.append(t)
-        return [t for s in excluded_sensors for t in temp.values[0] if s in t]
         
-                    
+        feat_list= [f for f in feat_names.values[0] if f not in
+                    [t for s in excluded_sensors for t in feat_names.values[0] if s in t]]
+        
+        # lista solo con i nomi delle feat
+        self._feat = [get_cleaned_name(feat_list[i]) for i in range(0, len(feat_list), 4)]
+        
+        return feat_list
+        
+def get_cleaned_name(name):
+    return re.search('(.+?)[#]', name).group(1).replace('android.sensor.', '')
+    
     
