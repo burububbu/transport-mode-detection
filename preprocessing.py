178e049be34bf0f7937fefe08ba824a69ebc2015
burububbu <aless97b@gmail.com>
2021-03-14 02:08:36 +0100

    preprocessing: add check nan samples

--- preprocessing.py
+++ preprocessing.py
@@ -1,30 +1,27 @@
-# FILE DIVISO I DUE PARTI
-# 1. analisi del dataset
-#       controllare se dataset è bilanciato
-#       controllare quandi sample ci sono (e quanti sono nan)
-#       (relatici grafici)
-
 import matplotlib.pyplot as plt
-import utils
+import numpy as np
 
-def check_balanced_dt(dt):
-    # remember -> group by without Nan
-    temp = dt.groupby(['target']).count()
-    pos = 1
-    for i in range(0,40,4):
-        plt.subplot(2, 5, pos)
-
-        plt.title(utils.clean_name(temp.iloc[:, i].name))
-        plt.bar(temp.index, temp.iloc[:, i])
-
+# Check how many nan values there are for each feature
+def checkSampleNotNull(data, feat,  plot=False):
+    tot_sample = data.shape[0]
+    nan_sample = tot_sample - data.iloc[:, :-1].count()
+    
+    if plot:
+        # multi bar plot to represent quantity of nan
+        n = np.arange(0, 10) # 10 feat
+        h = 0.25
         
-        pos = pos + 1
-
-    plt.show()
-    # TODO inoltre voglio sapere, per ogni feature (#mean), quanti sample non sono nan
-
-
-
-# 2. preprocessing
-#       effettuare preprocessing personalizzato in abse al tipo di model
-
+        # 4 bars group
+        plt.barh(n + 0.05, nan_sample[np.arange(0, 40, 4)], color='xkcd:azure', height=h-0.05) # blue mean
+        plt.barh(n + 0.25, nan_sample[np.arange(1, 40, 4)], color='xkcd:light red', height=h) # red min
+        plt.barh(n + 0.50, nan_sample[np.arange(2, 40, 4)], color='xkcd:medium green', height=h) # green max
+        plt.barh(n + 0.75, nan_sample[np.arange(3, 40, 4)], color='xkcd:dull pink', height=h) # pink std
+       
+        plt.title('NaN values for each feature')
+        plt.xlabel('number of Nan values')
+        plt.ylabel('sensors')
+        
+        plt.yticks(np.arange(h + 0.1, 10), feat)
+        plt.legend(['mean', 'min', 'max', 'std'])
+       
+    return nan_sample
