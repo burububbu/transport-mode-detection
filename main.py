2bc20b2a8b8c4b4c6e17c2d1441138f0358eefda
burububbu <aless97b@gmail.com>
2021-03-17 01:04:37 +0100

    add knn and boosting tree

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functools import update_wrapper
+
 from TDDataset import TDDataset
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
@@ -6,16 +6,15 @@ import numpy as np
 import pandas as pd 
 import preprocessing as prep
 import models as models
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.neighbors import KNeighborsClassifier
+
 
     
 path = '../dataset/dataset_5secondWindow.csv'
 excluded_sensor = ['light', 'pressure', 'magnetic_field','gravity','proximity']
 medians = [] #save here median values to use
 # i need it when i have to fill missing value in new raw data (model input)
-acc_rf = []
 
+miao = []
 results_columns = ['sensors', 'model type', 'score']
 results = pd.DataFrame(columns= results_columns)
 
@@ -32,70 +31,53 @@ def main():
     
     # assign best sensors (based on rf_each_sensor output)
     D1_s = ['accelerometer','sound','orientation'] # 12 feat
-    D2_s = ['accelerometer','sound','orientation','linear_acceleration','gyroscope'] # 20 feat
+    D2_s = ['accelerometer','sound','orientation','linear_acceleration','gyroscope', 'rotation_vector'] # 20 feat
     D3_s = dt.feat #36 feat
 
     # TEST on the three datasets // RANDOM FOREST, SVM, NB, KNN, NEURAL NET
     for s in [D1_s, D2_s, D3_s]:
         data = dt.get_train_test_feat(s)
-
-        # get_random_forest(*data, s) # RANDOM FOREST
+        get_random_forest(*data, s) # RANDOM FOREST
         get_SVM(*data, s) # SVM 
-        # get_nb(dt, d)
-        # get_knn(dt, s)
+        # get_bt(*data, s) # boosting tree
+        get_knn(*data, s) # KNN
      
+
+def get_knn(x_tr, x_te, y_tr, y_te, sensors):
+    global miao
+    # x_tr, x_te = prep.min_max_scaling(x_tr, x_te)
+    x_tr, x_te = prep.standardization(x_tr, x_te)
+    params = {'n_neighbors': np.arange(2, 10, 2)}
+    res = models.knn_(x_tr, x_te, y_tr, y_te, cv = True, param_grid= params)
+    # update results
+    update_res(pd.Series([sensors, 'KNN', res[1]], index = results_columns))
+    miao.append(res[2])
+     
+
 # use separate function to record the results
 def get_random_forest(x_tr, x_te, y_tr, y_te, sensors):
-    res = models.random_forest_(x_tr, x_te, y_tr, y_te, est=500)
+    res = models.random_forest_(x_tr, x_te, y_tr, y_te, est = 200)
     # update results
-    update_results(pd.Series([sensors, 'RF', res[1]], index = results_columns))
+    update_res(pd.Series([sensors, 'RF', res[1]], index = results_columns))
 
 
 def get_SVM(x_tr, x_te, y_tr, y_te, sensors):
     # STANDARDIZATION
     x_train, x_test = prep.standardization(x_tr, x_te)
-
     # search for model and cross validation
     params = {'C': np.arange(200, 800, step=200)}
     res = models.svm_(x_train, x_test, y_tr, y_te, cv = True, param_grid = params)
-    update_results(pd.Series([sensors, 'SVM', res[1]], index = results_columns))
+    update_res(pd.Series([sensors, 'SVM', res[1]], index = results_columns))
+
+def get_bt(x_tr, x_te, y_tr, y_te, sensors):
+    res = models.boosting_tree_(x_tr, x_te, y_tr, y_te, est=500)
+    update_res(pd.Series([sensors, 'Boosting tree', res[1]], index = results_columns))
 
 
-
-
-
-def update_results(se):
+def update_res(se):
     global results
     results = results.append(se, ignore_index=True)
 
-
-def get_knn(dt, sensors):
-    # 1. preprocessing
-    # STANDARDIZATION
-    global clf_nn
-    x_tr, x_te, y_tr, y_te = dt.get_train_test_feat(sensors)
-    x_tr, x_te = prep.min_max_scaling(x_tr, x_te)
-    
-    #x_tr, x_te = prep.do_pca(x_tr, x_te, sensors)
-    
-    knn = KNeighborsClassifier(n_neighbors=5)
-    
-    knn.fit(x_tr, y_tr)
-    
-    print(knn.score(x_te, y_te))
-
-
-    # penso che BAYES non vada bene
-def get_nb(dt, sensors):
-    x_tr, x_te, y_tr, y_te = dt.get_train_test_feat(sensors)
-    gnb = QuadraticDiscriminantAnalysis()
-    
-    gnb = gnb.fit(x_tr, y_tr)
-    y_pred = gnb.predict(x_te)
-    print("Number of mislabeled points out of a total", (y_te != y_pred).sum())
-    print(gnb.score(x_te, y_te))
-
-    
 
 def rf_each_sensor(dt, plot= True):
     ''' With random forest check accuracy for single sensor model'''
@@ -104,7 +86,7 @@ def rf_each_sensor(dt, plot= True):
     for name in dt.feat:
         data = dt.get_train_test_feat([name])
         res = models.random_forest(*data, est=500)
-        acc.append(res['accuracy'])
+        acc.append(res[1])
         
     sensors_accuracy = pd.Series(acc, dt.feat)
     
@@ -120,6 +102,7 @@ def first_preprocessing(dt):
     global uniq
     global nan_samples
     global medians
+    global describe
     # PREP on entire dataset
     # 1. check NaN values -----------------------------------------------------
     nan_samples = prep.check_nan_sample(dt.data, dt.feat, plot=False)
@@ -133,11 +116,10 @@ def first_preprocessing(dt):
     df = dt.data
     x_train, x_test, y_train, y_test = train_test_split(df.iloc[:, :-1], df['target'],  test_size=0.2, random_state=42 )
     # d = x_train.to_numpy()
-    
+    describe = df.describe()
     # PREP only on training
     # fill Nan values -> median
     # check speed#std distribution (bc there are many missing values)
-    # plot_distribution('speed', 3) # skewed, use median here
     
     x_train = prep.fill_NaN(x_train)
     medians = x_train.median()
@@ -170,15 +152,6 @@ def first_preprocessing(dt):
     dt.set_train_test(x_train, y_train, x_test, y_test)
     
     # --- finished first preprocessing
-    
-
-# def plot_distribution(sensor, i):
-#     if dt:
-#         feat = dt.get_data_feat([sensor]).iloc[:, i].dropna()
-#         print('mean', np.mean(feat))
-#         print('median', np.median(feat))
-#         plt.hist(feat, bins = np.arange(0, feat.max(), 0.05 ))
-
 
     
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,13 +1,14 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 
 from sklearn.svm import SVC
 
 
 def random_forest_(x_tr, x_te, y_tr, y_te, est = 100):
-    rf = RandomForestClassifier(n_estimators = est, random_state= 42)
+    rf = RandomForestClassifier(n_estimators=est, random_state=42)
     rf.fit(x_tr, y_tr)
     return rf, rf.score(x_te, y_te)
 
@@ -19,8 +20,28 @@ def svm_(x_tr, x_te, y_tr, y_te, cv=False, param_grid = {}):
         clf = GridSearchCV(svc, param_grid)
         clf.fit(x_tr, y_tr)
         svc = SVC(C=clf.best_params_['C'])
-    
+     
     svc.fit(x_tr, y_tr)
     
     return svc, svc.score(x_te, y_te)
+
+def boosting_tree_(x_tr, x_te, y_tr, y_te, est = 100):
+    bt = GradientBoostingClassifier(n_estimators = est, learning_rate= 0.01)
+    bt.fit(x_tr, y_tr)
+    return bt, bt.score(x_te, y_te)
+
+def knn_(x_tr, x_te, y_tr, y_te, cv= False, param_grid = {}):
+# cross validation ( search for hyperparameter C )
+    knn = KNeighborsClassifier()
+
+    if cv:
+        clf = GridSearchCV(knn, param_grid)
+        clf.fit(x_tr, y_tr)
+        print(clf.best_estimator_)
+        knn = KNeighborsClassifier(n_neighbors=clf.best_params_['n_neighbors'])
+    
+    knn.fit(x_tr, y_tr)
+    
+    return knn, knn.score(x_te, y_te), clf.cv_results_
+
 
