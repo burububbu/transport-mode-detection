9309a329fc1bbb502a6803f393fac49b4722291c
burububbu <aless97b@gmail.com>
2021-03-25 18:12:47 +0100

    update constants

--- cons.py
+++ cons.py
@@ -1,8 +1,26 @@
 PATH = '../dataset/dataset_5secondWindow.csv'
 TO_EXCLUDE = ['light', 'pressure', 'magnetic_field','gravity','proximity']
 
-RF_ESTIMATORS = 200
-KNN_GRID = {'n_neighbors': [2, 4, 6, 8, 10]}
-SVM_GRID = {'C': [200, 400, 600]}
 TEST_SIZE = 0.30
 
+# True to activate hyperparameters tuning
+TO_VAL = False
+NN_TO_VAL = False
+
+# RF params
+RF_ESTIMATORS = 200
+
+# SVM params
+SVM_GRID = {'C': [200, 400, 600]}
+C = {'D1': 600,'D4': 400,'D5': 400,}
+
+# KNN params
+KNN_GRID = {'n_neighbors': [2, 4, 6, 8, 10]}
+N_NEIGHBORS = {'D1': 4,'D4': 2,'D5': 2,}
+
+# setting for neural network
+HIDDEN_SIZE = {'D1': 50,'D4': 50,'D5': 100}
+L_RATE ={'D1': 0.01,'D4': 0.01,'D5': 0.01}
+EPOCHS = {'D1': 200,'D4': 200,'D5': 200}
+BATCH_SIZE = {'D1': 8,'D4': 16,'D5': 32}
+DROPOUT = {'D1': 0.1,'D4': 0.5,'D5': 0.2}
