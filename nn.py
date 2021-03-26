bfa9ff54272afe67327b760bd8c501048113eda3
burububbu <aless97b@gmail.com>
2021-03-26 01:36:48 +0100

    fixed things ( commit before an important change)

--- nn.py
+++ nn.py
@@ -1,25 +1,20 @@
-from cons import PATH, TEST_SIZE, TO_EXCLUDE
-import preprocessing as pre
-from TDDataset import TDDataset
-
-from sklearn.preprocessing import LabelEncoder
-import matplotlib.pyplot as plt
 import itertools
-import numpy as np
-
 import torch
+
+import numpy as np
+import pandas as pd
 from torch import nn
-from torch.utils.data import Dataset, DataLoader, Subset
+from torch.utils.data import Dataset, DataLoader
 
 torch.backends.cudnn.benchmark = False
 
-# class that represent dataset
-class TMDataset(Dataset):
+# then the instances will be passed to dataloader 
+class NNDataset(Dataset):
     def __init__(self, x, y):
         self.num_classes = len(np.unique(y))
         self.X = torch.FloatTensor(x)
         self.y = torch.LongTensor(y)
-    
+
     def __len__(self):
         return self.X.shape[0]
 
@@ -28,151 +23,225 @@ class TMDataset(Dataset):
 
 
 class NeuralNet(nn.Module):
+    """ Neural network model"""
     def __init__(self, in_size, hidden_size, out_size, dropout):
         super(NeuralNet, self).__init__()
-        
+
         self.input_size = in_size
         self.hidden_size = hidden_size
         self.output_size = out_size
 
+        # self.model = nn.Sequential(
+        #     nn.Dropout(dropout),
+        #     nn.Linear(in_size, hidden_size), #h1
+        #     nn.ReLU(),
+        #     nn.Dropout(dropout),
+        #     nn.Linear(hidden_size, hidden_size), #h2
+        #     nn.ReLU(),
+        #     nn.Dropout(dropout),
+        #     nn.Linear(hidden_size, hidden_size), #h3
+        #     nn.ReLU(), 
+        #     nn.Linear(hidden_size, out_size), #out
+        # )
+
         self.model = nn.Sequential(
-            nn.Linear(in_size, hidden_size), #h1
+            nn.Dropout(dropout),
+            nn.Linear(in_size, hidden_size),  #h1
             nn.ReLU(),
             nn.Dropout(dropout),
-            nn.Linear(hidden_size, hidden_size), #h2
+            nn.Linear(hidden_size, hidden_size),  #h2
             nn.ReLU(),
             nn.Dropout(dropout),
-            nn.Linear(hidden_size, out_size), #out
+            nn.Linear(hidden_size, hidden_size),  #h3
+            nn.ReLU(),
+            nn.Linear(hidden_size, out_size),  #out
         )
 
     def forward(self, x):
         out = self.model(x)
         return out
 
-
-def train_loop(dataloader, model, loss_fn, optimizer, epochs, device):
-    # training mode
-    model.train()
+def train_loop(dataloader, model, loss_fn, optimizer, epochs, device, valloader = None):
+    """
+        For each epoch iterate over dataset searching for optimal results.
+        If valloader != None then for each epoch will be tested the model. 
     
-    n_batch = len(dataloader)
-    loss_mean_epochs = []
-    acc = 0
-    print('Evaluating ...')
-    loss_values = [] # tutte le loss per ogni batch
-    for e in range(epochs):
-        # print('Starting epoch {}'.format(e+1))
-        for data, targets in dataloader:
-            data = data.to(device)
-            targets = targets.to(device)
+        Return: (model, list of all loss values, list of loss values means for epoch)
+    """
+    model.train()
 
-            # forward pass + loss computation
+    n_batch = len(dataloader)
+
+    loss_mean_epochs = []
+    loss_values = []
+
+    print('Evaluating ...')
+    for e in range(epochs):
+        acc = 0
+        for data, targets in dataloader:
+            data, targets = data.to(device), targets.to(device)
+
+            # forward pass
             pred = model(data)
 
+            # loss computation
             loss = loss_fn(pred, targets)
             loss_values.append(loss)
             acc = acc + loss.item()
 
+            # backpropagation
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
         loss_mean_epochs.append(acc/n_batch) 
-    # model, all loss values for ever batch (len(batches) * epoch), mean loss for every epoch
+
+        print(' epoch {}'.format(e+1))
+        print('\tloss: {}'.format(acc/n_batch))
+
+        if valloader:
+            print('\t test loss: {}'.format(test_loop(valloader, model, loss_fn, device)[1]))
+            model.train()
+
     return model, loss_values, loss_mean_epochs
 
+
 def test_loop(dataloader, model, loss_fn, device):
-    # evaluation mode
+    """
+        Test model, iterate over the val/test dataset
+
+        Return (accuracy, mean test loss)  
+    """
     model.eval()
 
     test_loss, correct = 0, 0
-    
-    size = len(dataloader.dataset)
+
     with torch.no_grad():
         for data, targets in dataloader:
             data, targets = data.to(device), targets.to(device)
-           
+
             pred = model(data)
 
-            test_loss  = test_loss +  loss_fn(pred, targets).item()
-            correct = correct +  (pred.argmax(1) == targets).type(torch.float).sum().item()
+            test_loss = test_loss + loss_fn(pred, targets).item()
+            correct = correct + (pred.argmax(1) == targets).type(torch.float).sum().item() # correct predictions
 
-    # media della loss di ogni batch (che forma il dataset)
+    # average of minibatches loss values  
     test_loss = test_loss/len(dataloader)
-    # numero predizioni corrette sul totale del dataset
-    correct = correct/size
-    
-    # accuracy, avg loss
-    return correct*100, test_loss
+
+    return correct/len(dataloader.dataset), test_loss
 
 
-if __name__ == '__main__':
-    # setting the device to use
+def get_nn(x_tr, x_te, y_tr, y_te, v=False, hs=10, lr=0.01, epochs=100, bs=64, drop=0.2):
+    """
+        Train mlp. If v = True then search for best hyperparameters else train with given parameters.
+
+        Return: (model, score) if v = False, (results df, best score) otherwise
+    """
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Using {} device".format(device))
 
-    # set here hyperparameters
-    # TODO set hyp (these are examples)
-    # hidden_sizes = [10, 100, 300, 600] # numero neuroni del mio hidden layer
-    # nums_epochs = [100, 500]
-    # batch_sizes = [16, 32, 65]
+    torch.manual_seed(42)
+    np.random.seed(42)
+    torch.set_deterministic(True)
 
-    # 10, 100,
-    hidden_sizes = [100, 300, 600] # numero neuroni del mio hidden layer
-    nums_epochs = [200, 500]
-    batch_sizes = [16, 32]
-    learning_rate = [0.001, 0.01]
-    
-    hyperparameters = itertools.product(hidden_sizes, nums_epochs, batch_sizes, learning_rate)
-
-    # load dataset
-    dt = TDDataset(PATH, TO_EXCLUDE)
-
-    le = LabelEncoder()
-    le.fit(dt.data['target'])
-    # 1a. preprocessing
-    dt.remove_sensor_feat('step_counter')
-    dt.split_train_test(TEST_SIZE, prep = True) # fill Nan with median, drop duplicates
-
-    D1 = ['accelerometer','sound', 'orientation']   
-    D4 = ['accelerometer','sound','orientation','linear_acceleration','gyroscope', 'rotation_vector']
-    x_tr, x_te, y_tr, y_te = dt.get_train_test_sensors(D1)
-
-    x_tr_st, x_te_st = pre.standardization(x_tr, x_te)
-    
-    # encording
-    y_tr_enc = le.transform(y_tr) 
-    y_te_enc = le.transform(y_te)
-    
-    training_data = TMDataset(x_tr_st.to_numpy(), y_tr_enc)
-    test_data = TMDataset(x_te_st.to_numpy(), y_te_enc)
+    # create one Dataset object for training set and another for test
+    training_data = NNDataset(x_tr.to_numpy(), y_tr)
+    test_data = NNDataset(x_te.to_numpy(), y_te)
 
     val_loader = DataLoader(test_data, batch_size=1, shuffle=True)
 
-    # for della cross val
-    for hidden_size, epochs, batch_size, l_rate in hyperparameters:
-        torch.manual_seed(42)
-        np.random.seed(42)
-        torch.set_deterministic(True)
-        print('With hidden-size {}, learning_rate {}, epochs {}, batch size {}'.format(hidden_size, l_rate, epochs, batch_size))
-        
-        train_loader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+    if v: # search for best hyperparams
+        results = pd.DataFrame(columns=['params', 'train_acc', 'test_acc'])
 
-        model = NeuralNet(training_data.X.shape[1], hidden_size, training_data.num_classes, 0.2)
+        # hyperparameters space
+        hidden_sizes = [20, 50, 100]
+        nums_epochs = [50, 100]
+        batch_sizes = [8, 16, 32]
+        learning_rate = [0.1, 0.01]
+        dropout = [0.2, 0.5]
+
+        hyperparameters = itertools.product(hidden_sizes, nums_epochs, batch_sizes, learning_rate, dropout)
+
+        print('Starting hyperparameters tuning...')
+        for hidden_size, l_rate, epochs, batch_size, dropout in hyperparameters:
+            
+            train_loader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+
+            model = NeuralNet(training_data.X.shape[1], hidden_size, training_data.num_classes, dropout)
+            model.to(device)
+
+            loss_fn = nn.CrossEntropyLoss()
+            optimizer = torch.optim.SGD(model.parameters(), lr=l_rate)
+
+            # train the model 
+            model, _, means = train_loop(train_loader, model, loss_fn, optimizer, epochs, device)
+            # plt.plot(mean)
+
+            score, _ = test_loop(val_loader, model, loss_fn, device)
+            train_acc = test_loop(train_loader, model, loss_fn, device)[0]
+            
+            print('With hidden-size {}, learning_rate {}, epochs {}, batch size {}, dropout {}'.format(hidden_size, l_rate, epochs, batch_size, dropout))
+            print('Train set accuracy: {}'.format(train_acc))
+            print('Val set accuracy: {}'.format(score))
+
+            # append information to results dataframe
+            results.append(
+                [[{'hs': hidden_size, 'lr': l_rate, 'ep': epochs, 'b':batch_size, 'dr': dropout},
+                    train_acc,
+                    score]
+                    ])
+
+            return results, results['test_acc'].max()
+
+    else:  # here there isn't hyperparams tuning
+        print('With hidden-size {}, learning_rate {}, epochs {}, batch size {}'.format(hs, lr, epochs, bs)) 
+        train_loader = DataLoader(training_data, batch_size= bs, shuffle=True)
+
+        model = NeuralNet(training_data.X.shape[1], hs, training_data.num_classes, drop)
         model.to(device)
 
         loss_fn = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=l_rate)
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr)\
 
-        model, loss_values, _ = train_loop(train_loader, model, loss_fn, optimizer, epochs, device)
-
+        model, _, means = train_loop(train_loader, model, loss_fn, optimizer, epochs, device, valloader=val_loader)
         score, _ = test_loop(val_loader, model, loss_fn, device)
+        #plt.plot(means)
+        print('Val set accuracy: {}'.format(score))
+        
+        return model, score
 
-        print('With hidden-size {}, learning_rate {}, epochs {}, batch size {}'.format(hidden_size, l_rate, epochs, batch_size)) 
-        print('accuracy on train = {}'.format(test_loop(train_loader, model, loss_fn, device)[0]))
-        print('accuracy on test = {}'.format(score))
 
+def temp(training_data, val_loader, hidden_size, l_rate, epochs, batch_size, dropout, device, v = False):
+        train_loader = DataLoader(training_data, batch_size=batch_size, shuffle=True)       
     
+        model = NeuralNet(training_data.X.shape[1], hidden_size, training_data.num_classes, dropout)
+        model.to(device)
 
+        loss_fn = nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=l_rate)
 
+        # train the model 
+        model, _, means = train_loop(train_loader, model, loss_fn, optimizer, epochs, device)
+        # plt.plot(mean)
 
+        val_score, val_avg_loss  = test_loop(val_loader, model, loss_fn, device)
+        train_score, train_avg_loss = test_loop(train_loader, model, loss_fn, device)[0]
+    
+        print('With hidden-size {}, learning_rate {}, epochs {}, batch size {}, dropout {}'.format(hidden_size, l_rate, epochs, batch_size, dropout))
+        print('Train set accuracy: {}, Avg loss: {}'.format(train_score, train_avg_loss))
+        print('Val set accuracy: {}, Avg loss: {}'.format(val_score, val_avg_loss))
 
+        # return information to append to the final results in hyperparameters search
+        if v:
+            return [
+                {'hs': hidden_size, 'lr': l_rate, 'ep': epochs, 'b':batch_size, 'dr': dropout},
+                train_score,
+                val_score
+                ]
+        
+        
+        
+        # results.append(
+        #     [
+        #         ])
